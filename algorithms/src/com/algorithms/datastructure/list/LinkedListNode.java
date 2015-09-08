1e9012aaa1b2fcef9a75d2f2cbd9160c41caf34c
srirambtechit <srirambtecit@gmail.com>
2015-09-08 20:34:42 +0530

    code formatted.

--- algorithms/src/com/algorithms/datastructure/list/LinkedListNode.java
+++ algorithms/src/com/algorithms/datastructure/list/LinkedListNode.java
@@ -1,17 +1,17 @@
 package com.algorithms.datastructure.list;
 
 public class LinkedListNode {
-    Integer data;
-    LinkedListNode next;
+	Integer data;
+	LinkedListNode next;
 
-    public LinkedListNode(Integer data, LinkedListNode next) {
-	this.data = data;
-	this.next = next;
-    }
+	public LinkedListNode(Integer data, LinkedListNode next) {
+		this.data = data;
+		this.next = next;
+	}
 
-    public String toString() {
-	if (next == null)
-	    return data.toString();
-	return data + ", " + next.toString();
-    }
+	public String toString() {
+		if (next == null)
+			return data.toString();
+		return data + ", " + next.toString();
+	}
 }
--- algorithms/src/com/algorithms/datastructure/list/ListApplication.java
+++ algorithms/src/com/algorithms/datastructure/list/ListApplication.java
@@ -4,86 +4,86 @@ import java.util.List;
 
 public class ListApplication {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	testFrontBackSplit();
+		testFrontBackSplit();
 
-	testRemoveDupsSortedList();
+		testRemoveDupsSortedList();
 
-	testMoveNode();
+		testMoveNode();
 
-	testAlternateSplit();
+		testAlternateSplit();
 
-    }
+	}
 
-    public static void testAlternateSplit() {
-	SingleLinkedList list = new SingleLinkedList();
-	list.add(81);
-	list.add(42);
-	list.add(53);
-	list.add(44);
-	list.add(25);
-	list.add(76);
+	public static void testAlternateSplit() {
+		SingleLinkedList list = new SingleLinkedList();
+		list.add(81);
+		list.add(42);
+		list.add(53);
+		list.add(44);
+		list.add(25);
+		list.add(76);
 
-	System.out.println("original : " + list);
+		System.out.println("original : " + list);
 
-	List<SingleLinkedList> result = SingleLinkedList.alternateSplit(list);
-	System.out.println("subLists : " + result);
-    }
+		List<SingleLinkedList> result = SingleLinkedList.alternateSplit(list);
+		System.out.println("subLists : " + result);
+	}
 
-    public static void testFrontBackSplit() {
-	SingleLinkedList list = new SingleLinkedList();
-	list.add(81);
-	list.add(42);
-	list.add(53);
-	list.add(44);
-	list.add(25);
-	list.add(76);
+	public static void testFrontBackSplit() {
+		SingleLinkedList list = new SingleLinkedList();
+		list.add(81);
+		list.add(42);
+		list.add(53);
+		list.add(44);
+		list.add(25);
+		list.add(76);
 
-	System.out.println("inputList     : " + list);
+		System.out.println("inputList     : " + list);
 
-	List<SingleLinkedList> lists = SingleLinkedList.frontBackSplit(list);
-	System.out.println("frontBackList : " + lists);
-    }
+		List<SingleLinkedList> lists = SingleLinkedList.frontBackSplit(list);
+		System.out.println("frontBackList : " + lists);
+	}
 
-    public static void testRemoveDupsSortedList() {
+	public static void testRemoveDupsSortedList() {
 
-	SingleLinkedList sortedList = new SingleLinkedList();
-	sortedList.add(1);
-	sortedList.add(2);
-	sortedList.add(2);
-	sortedList.add(3);
-	sortedList.add(5);
-	sortedList.add(7);
-	sortedList.add(7);
-	sortedList.add(7);
-	sortedList.add(9);
+		SingleLinkedList sortedList = new SingleLinkedList();
+		sortedList.add(1);
+		sortedList.add(2);
+		sortedList.add(2);
+		sortedList.add(3);
+		sortedList.add(5);
+		sortedList.add(7);
+		sortedList.add(7);
+		sortedList.add(7);
+		sortedList.add(9);
 
-	System.out.println("sortedList : " + sortedList);
-	sortedList.removeDupsSortedList();
-	System.out.println("sortedList : " + sortedList);
-    }
+		System.out.println("sortedList : " + sortedList);
+		sortedList.removeDupsSortedList();
+		System.out.println("sortedList : " + sortedList);
+	}
 
-    public static void testMoveNode() {
-	SingleLinkedList listOne = new SingleLinkedList();
-	listOne.add(1);
-	listOne.add(2);
-	listOne.add(3);
+	public static void testMoveNode() {
+		SingleLinkedList listOne = new SingleLinkedList();
+		listOne.add(1);
+		listOne.add(2);
+		listOne.add(3);
 
-	SingleLinkedList listTwo = new SingleLinkedList();
-	listTwo.add(5);
-	listTwo.add(6);
-	listTwo.add(7);
-	listTwo.add(9);
+		SingleLinkedList listTwo = new SingleLinkedList();
+		listTwo.add(5);
+		listTwo.add(6);
+		listTwo.add(7);
+		listTwo.add(9);
 
-	System.out.println("listOne : " + listOne);
-	System.out.println("listTwo : " + listTwo);
+		System.out.println("listOne : " + listOne);
+		System.out.println("listTwo : " + listTwo);
 
-	SingleLinkedList.moveNode(listOne, listTwo);
+		SingleLinkedList.moveNode(listOne, listTwo);
 
-	System.out.println("listOne : " + listOne);
-	System.out.println("listTwo : " + listTwo);
+		System.out.println("listOne : " + listOne);
+		System.out.println("listTwo : " + listTwo);
 
-    }
+	}
 
 }

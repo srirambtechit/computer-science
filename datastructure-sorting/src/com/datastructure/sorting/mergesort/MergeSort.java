9613206b8b5ad382fba79a8fb9772559050a2a02
srirambtechit <srirambtecit@gmail.com>
2015-10-01 13:29:12 +0530

    MergeSort algorithm added

---
+++ datastructure-sorting/src/com/datastructure/sorting/mergesort/MergeSort.java
@@ -0,0 +1,57 @@
+package com.datastructure.sorting.mergesort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+	int[] data = { 3, 2, 8, 1, 7, 9, 5, 0 };
+
+	System.out.println(Arrays.toString(data));
+
+	MergeSort.mergeSort(data);
+
+	System.out.println(Arrays.toString(data));
+    }
+
+    public static void mergeSort(int[] data) {
+	if (data.length < 2) {
+	    return;
+	}
+
+	int mid = data.length / 2;
+	int[] leftList = new int[mid];
+	int[] rightList = new int[data.length - mid];
+
+	System.arraycopy(data, 0, leftList, 0, mid);
+	System.arraycopy(data, mid, rightList, 0, data.length - mid);
+
+	mergeSort(leftList);
+	mergeSort(rightList);
+	merge(leftList, rightList, data);
+    }
+
+    private static void merge(int[] leftList, int[] rightList, int[] data) {
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (i < leftList.length && j < rightList.length) {
+	    if (leftList[i] < rightList[j]) {
+		data[k++] = leftList[i++];
+	    } else {
+		data[k++] = rightList[j++];
+	    }
+	}
+
+	while (i < leftList.length) {
+	    data[k++] = leftList[i++];
+	}
+
+	while (j < rightList.length) {
+	    data[k++] = rightList[j++];
+	}
+
+    }
+
+}
---
+++ datastructure-sorting/src/com/datastructure/sorting/mergesort/test/MergeSortTest.java
@@ -0,0 +1,52 @@
+package com.datastructure.sorting.mergesort.test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.datastructure.sorting.mergesort.MergeSort;
+
+public class MergeSortTest {
+
+    @Test
+    public void testMergeSort_1() {
+	System.out.println("Case 1: Array with pass scenario");
+	int[] input = { 3, 2, 8, 1, 7, 9, 5, 0 };
+	int[] output = new int[input.length];
+	System.arraycopy(input, 0, output, 0, input.length);
+
+	MergeSort.mergeSort(output);
+	Arrays.sort(input);
+
+	assertArrayEquals("Both are equal", input, output);
+    }
+
+    @Test
+    public void testMergeSort_2() {
+	System.out.println("Case 2: Empty array");
+	int[] input = {};
+	int[] output = new int[input.length];
+	System.arraycopy(input, 0, output, 0, input.length);
+
+	MergeSort.mergeSort(output);
+	Arrays.sort(input);
+
+	assertArrayEquals("Both are equal", input, output);
+    }
+
+    @Test
+    public void testMergeSort_3() {
+	System.out.println("Case 3: Array with duplicate elements");
+	int[] input = { 3, 2, 8, 3, 7, 9, 5, -2, 0, 3, 7, 2, 6, 1, -1, 9 };
+	int[] output = new int[input.length];
+	System.arraycopy(input, 0, output, 0, input.length);
+
+	MergeSort.mergeSort(output);
+	Arrays.sort(input);
+
+	assertArrayEquals("Both are equal", input, output);
+    }
+
+}

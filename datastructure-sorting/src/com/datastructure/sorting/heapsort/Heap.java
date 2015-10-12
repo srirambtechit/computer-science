6f2c408a5ca2c47423979932eded2a5add528eed
Sriram <srirambtecit@gmail.com>
2015-10-12 10:06:17 +0530

    Heap sort final code added

--- datastructure-sorting/src/com/datastructure/sorting/heapsort/Heap.java
+++ datastructure-sorting/src/com/datastructure/sorting/heapsort/Heap.java
@@ -6,40 +6,14 @@ import com.datastructure.sorting.util.Type;
 public class Heap {
 
     public static <T> void sort(Comparable<T>[] a) {
-	int n = a.length;
-	for (int k = n / 2; k >= 1; k--) {
+	int n = a.length - 1;
+	for (int k = n / 2; k > 0; k--) {
 	    sink(a, k, n);
 	}
-	while (n > 1) {
+
+	while (n > 0) {
 	    SortingUtil.swap(a, 1, n);
 	    sink(a, 1, --n);
-	}
-    }
-
-    /**
-     * Level of N nodes in tree
-     * Ex: L = 1 when n = 1
-     * L = 2 when n = 2,3
-     * L = 3 when n = 4, 5, 6, 7
-     * L = 4 when n = 8, 9, 10, 11, 12, 13, 14, 15
-     * L = k when n = 2*k, 2*k+1, 2*k+2, ..., 2^k-1
-     * 
-     * @param n
-     * @return
-     */
-    private static int levelOf(int n) {
-	int level = 0;
-	while (n > 0) {
-	    level++;
-	    n = n >> 1;
-	}
-	return level;
-    }
-
-    private static <T> void swim(Comparable<T>[] a, int k, int n) {
-	while (k > 1 && SortingUtil.less(a[k / 2], a[k])) {
-	    SortingUtil.swap(a, k / 2, k);
-	    k = k / 2;
 	}
     }
 
@@ -56,11 +30,11 @@ public class Heap {
     }
 
     public static void main(String[] args) {
-	SortingUtil.display(new Integer[] { -1, 5, 1, 4, 2, 8, 7 }, Type.Heap);
-	// SortingUtil.display(new Integer[] { 6, 5, 3, 1, 8, 7, 2, 4 },
-	// Type.Heap);
-	// SortingUtil.display(new Integer[] { 8, -1, 6, 0, 9, 4, 2, -57445,
-	// 3254, 245, 21017274 }, Type.Heap);
-	// SortingUtil.display(new Integer[] {}, Type.Heap);
+	SortingUtil.display(new Integer[] { null, 4, 8, 3, 0, 9, -1, 5 }, Type.Heap);
+	SortingUtil.display(new Integer[] { null, 5, 1, 4, 2, 8, 7 }, Type.Heap);
+	SortingUtil.display(new Integer[] { null, 6, 5, 3, 1, 8, 7, 2, 4 }, Type.Heap);
+	SortingUtil.display(new Integer[] { null, 6, 5, 3, 1, 8, 5, 2, 4 }, Type.Heap);
+	SortingUtil.display(new Integer[] { null, 8, -1, 6, 0, 9, 4, 2, -57445, 3254, 245, 21017274 }, Type.Heap);
+	SortingUtil.display(new Integer[] {}, Type.Heap);
     }
 }
